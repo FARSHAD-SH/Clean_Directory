3efd9528857eb3fe9ff17c3cb1ac9934d1c0b4d5
FARSHAD-SH <farshad.shamlu@gmail.com>
2022-07-25 20:07:00 +0200

    update OrganizeFiles to get directory in obj call

--- src/Clean.py
+++ src/Clean.py
@@ -1,22 +1,20 @@
-import shutil
-from pathlib import Path
 import json
+import shutil
+from ctypes import Union
+from pathlib import Path
+from typing import Union
+
 from loguru import logger
 
 from src.data import DATA_DIR
 from src.utils.io import read_json
-
 
 class OrganizeFiles():
     """
     This class is used to organize files in a directory by
     moving files into directories based on extension.
     """
-    def __init__(self, directory) -> None:
-
-        self.directory = Path(directory)
-        if not self.directory.exists():
-            raise FileNotFoundError(f"{self.directory}Directory does not exist.")
+    def __init__(self):
 
         ext_dirs = read_json(DATA_DIR / "extensions.json")
         self.extentions = {}
@@ -24,18 +22,18 @@ class OrganizeFiles():
             for ext1 in ext:
                 self.extentions[ext1] = dir
 
-        # print(self.extentions)
-
-
-
-    def __call__(self):
+    def __call__(self, directory: Union[str, Path]):
         """ Organize files in directory by moving
          them into directories based on extension.
         """
-        logger.info(f"Organizing files in {self.directory} ...")
 
+        directory = Path(directory)
+        if not directory.exists():
+            raise FileNotFoundError(f"{directory}Directory does not exist.")
+
+        logger.info(f"Organizing files in {directory} ...")
         file_extension = []
-        for file in self.directory.iterdir():
+        for file in directory.iterdir():
 
         # check file path is file or directory and ignore directories
             if file.is_dir():
@@ -48,16 +46,15 @@ class OrganizeFiles():
         # move files
             file_extension.append(file.suffix)
             if file.suffix not in self.extentions:  # files that are not blong to file_type_des move to other
-                DEST_DIR = self.directory / 'other'
+                DEST_DIR = directory / 'other'
             else:
-                DEST_DIR = self.directory / self.extentions[file.suffix]
+                DEST_DIR = directory / self.extentions[file.suffix]
             logger.info(f"Moving {file} to {DEST_DIR} ...")
             DEST_DIR.mkdir(exist_ok=True)
 
             shutil.move(str(file), str(DEST_DIR))
 
-
 if __name__ == "__main__":
-    org_files = OrganizeFiles('/home/farshad/Downloads')
-    org_files()
+    org_files = OrganizeFiles()
+    org_files('/home/farshad/Downloads')
     logger.info('Done!')
